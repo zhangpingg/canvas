0c3f437416b8a542ea33ac163e515f9b60157500
zhangping <2425238116@qq.com>
2024-08-20 18:04:59 +0800

    feat: 放大缩小

--- src/pages/test/index.vue
+++ src/pages/test/index.vue
@@ -1,36 +1,53 @@
 <template>
     <div>
         <canvas id="canvas"></canvas>
+        <input type="range" id="scale-range" min="0.1" max="1.5" step="0.01" value="0.3" />
     </div>
 </template>
 
 <script setup>
 import { onMounted } from 'vue';
 
+const drawImageByScale = (canvas, context, img, scale) => {
+    var imgWidth = 640 * scale;
+    var imgHeight = 640 * scale;
+
+    var dx = canvas.width / 2 - imgWidth / 2;
+    var dy = canvas.height / 2 - imgHeight / 2;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(img, dx, dy, imgWidth, imgHeight);
+};
+
 onMounted(() => {
-    var c = document.getElementById('canvas');
-    var ctx = c.getContext('2d');
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+    var slider = document.getElementById('scale-range');
     var img = new Image();
 
-    c.width = 200;
-    c.height = 200;
-    img.src = '/canvas/src/assets/people.jpg';
-    img.onload = function () {
-        //ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-        ctx.drawImage(img, 0, 0, 320, 320, 0, 0, c.width / 2, c.height / 2);
-    };
+    canvas.width = 300;
+    canvas.height = 300;
 
-    /*
-    1)在画布上定位图像
-        context.drawImage(img,x,y,width,height);
-    2)剪切图像
-        context.drawImage(img,sx,sy,swidth,sheight,x,y,width,height);
-    */
+    var scale = slider.value;
+
+    img.src = '/canvas/src/assets/people.jpg';
+    // 图片加载完后，将其显示在canvas中
+    img.onload = function () {
+        drawImageByScale(canvas, context, img, scale);
+        slider.onmousemove = function () {
+            scale = slider.value;
+            drawImageByScale(canvas, context, img, scale);
+        };
+    };
 });
 </script>
 
 <style scoped lang="less">
 canvas {
     border: 1px solid #000;
+}
+input {
+    display: block;
+    margin-top: 20px;
+    width: 300px;
 }
 </style>
